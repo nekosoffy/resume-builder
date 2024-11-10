17267ca028461bac49224306322051a8ad70329e
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-11-10 00:54:25 -0300

    refactor: adjust name component

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,93 +1,29 @@
-import { useState } from 'react';
+import InputList from './InputList.jsx';
+import Section from './Section.jsx';
+import useFields from '../useFields.js';
+import { names } from '../inputData.js';
 
 export default function Name() {
-  const [edit, setEdit] = useState(false);
-  const [name, setName] = useState({
-    forename: 'Forename',
-    surname: 'Surname',
-  });
-
-  function handleNameChange(e) {
-    const { id, value } = e.target;
-    setName((prev) => ({ ...prev, [id]: value }));
-  }
-
-  function handleClick(e) {
-    e.preventDefault();
-
-    if (!edit) {
-      setName({
-        forename: name.forename === 'Forename' ? '' : name.forename,
-        surname: name.surname === 'Surname' ? '' : name.surname,
-      });
-    } else {
-      setName({
-        forename: name.forename.trim() === '' ? 'Forename' : name.forename,
-        surname: name.surname.trim() === '' ? 'Surname' : name.surname,
-      });
-    }
-
-    setEdit(!edit);
-  }
+  const { edit, data, handleClick, fields } = useFields(names);
 
   return (
-    <>
-      {edit ? (
-        <section className="section name edit">
-          <form
-            id="name-form"
-            onSubmit={handleClick}
-          >
-            <fieldset>
-              <legend className="section-title">
-                <div>Name section</div>
-              </legend>
-              <ul>
-                <li className="input-wrapper">
-                  <label htmlFor="forename">
-                    <div>Forename</div>
-                  </label>
-                  <input
-                    id="forename"
-                    value={name.forename}
-                    placeholder="e.g. Jane"
-                    onChange={handleNameChange}
-                  />
-                </li>
-                <li className="input-wrapper">
-                  <label htmlFor="forename">
-                    <div>Surname</div>
-                  </label>
-                  <input
-                    id="surname"
-                    value={name.surname}
-                    placeholder="e.g. Doe"
-                    onChange={handleNameChange}
-                  />
-                </li>
-              </ul>
-            </fieldset>
-          </form>
-          <button
-            type="submit"
-            form="name-form"
-            className="done-btn"
-          >
-            Done
-          </button>
-        </section>
-      ) : (
-        <section>
-          <button
-            className="section name"
-            onClick={handleClick}
-          >
-            <h1>
-              {name.forename} <span className="bold">{name.surname}</span>
-            </h1>
-          </button>
-        </section>
-      )}
-    </>
+    <Section
+      sectionName="name"
+      formStructure={
+        <fieldset>
+          <legend className="section-title">
+            <div>Name Section</div>
+          </legend>
+          <InputList fields={fields} />
+        </fieldset>
+      }
+      displayStructure={
+        <h1>
+          {data.forename} <span className="bold">{data.surname}</span>
+        </h1>
+      }
+      onSubmit={handleClick}
+      edit={edit}
+    />
   );
 }
