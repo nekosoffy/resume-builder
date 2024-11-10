7062ac64f94aee30ce54d05b7359d7a53316336c
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-11-09 23:12:23 -0300

    refactor: separate form input data into arrays

---
+++ src/components/data.jsx
@@ -0,0 +1,14 @@
+const names = [
+  {
+    id: 'forename',
+    label: 'Forename',
+    placeholder: 'e.g. Jane',
+  },
+  {
+    id: 'surname',
+    label: 'Surname',
+    placeholder: 'e.g. Doe',
+  },
+];
+
+export { names };
